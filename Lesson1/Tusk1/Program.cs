5bc3b0c7af640a417dde3cf100d353994455db6b
Bogdan <123@explane.ru>
2024-03-19 23:12:15 +0500

    add some value

--- Lesson1/Tusk1/Program.cs
+++ Lesson1/Tusk1/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, world!");
+﻿// Console.WriteLine("Hello, world!");
+
+
+int red = 99;
+Console.WriteLine(red);
+
+double a = 3.55;
+char b = '?';
+bool c = true;
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
