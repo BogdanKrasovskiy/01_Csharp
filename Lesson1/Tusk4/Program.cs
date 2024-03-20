3d7da5dacce8c40a1657d3486d01d6911b7b44a7
Bogdan <123@explane.ru>
2024-03-20 21:26:14 +0500

    add tusk4

---
+++ Lesson1/Tusk4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 2;
+int b = 4;
+int c = 16;
+int d = 3;
+int e = 10;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine (max);
